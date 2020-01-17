cd942b1226e14ecc6115cad6e21aa63e9deeed9b
L. Alberto Giménez <alberto.gimenez@capside.com>
2020-01-17 17:56:43 +0100

    implement read

--- happyb.c
+++ happyb.c
@@ -3,6 +3,7 @@
 #include <linux/module.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 
 #define MAX_NAME 20
 
@@ -28,7 +29,24 @@ static int happyb_release(struct inode *inode, struct file *file)
 
 static ssize_t happyb_read(struct file *file, char *buf, size_t count, loff_t *f_pos)
 {
-	return 0;
+	char msg[128];
+	int len;
+
+	pr_info("count: %ld, fpos: %lld", count, *f_pos);
+
+	len = snprintf(msg, sizeof msg, "Happy birthday, %s!\n", name);
+	if (*f_pos >= len) {
+		return 0;
+	}
+
+	if (count > len) {
+		count = len;
+	}
+
+	copy_to_user(buf, msg, count);
+	*f_pos += count;
+
+	return count;
 }
 
 static ssize_t happyb_write(struct file *file, const char *buf, size_t count, loff_t *f_pos)
@@ -48,6 +66,7 @@ struct miscdevice happyb_dev = {
     .minor = MISC_DYNAMIC_MINOR,
     .name = "happy-birthday",
     .fops = &happyb_fops,
+	.mode = 0666,
 };
 
 
