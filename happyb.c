9c7cba411be43ef155bf8f57cd99b5071e42ba9c
L. Alberto Giménez <alberto.gimenez@capside.com>
2020-01-17 16:56:09 +0100

    basic misc device structure

--- happyb.c
+++ happyb.c
@@ -1,20 +1,77 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+
+#define MAX_NAME 20
 
 MODULE_DESCRIPTION("Happy Birthday Module");
 MODULE_AUTHOR("L. Alberto Giménez <alberto.gimenez@capside.com>");
 MODULE_LICENSE("GPL");
 
+static char *param_name = "my friend";
+
+static char name[MAX_NAME];
+
+/*
+static int happyb_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static int happyb_release(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+*/
+
+static ssize_t happyb_read(struct file *file, char *buf, size_t count, loff_t *f_pos)
+{
+	return 0;
+}
+
+static ssize_t happyb_write(struct file *file, const char *buf, size_t count, loff_t *f_pos)
+{
+	return 0;
+}
+
+struct file_operations happyb_fops = {
+    read: happyb_read,
+    write: happyb_write,
+    /*open: happyb_open,
+    release: happyb_release,
+	*/
+};
+
+struct miscdevice happyb_dev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "happy-birthday",
+    .fops = &happyb_fops,
+};
+
+
 static int happyb_init(void)
 {
-        pr_debug("Hi\n");
-        return 0;
+	int res;
+
+	strncpy(name, param_name, MAX_NAME);
+	pr_info("Happy birthday, %s!\n", name);
+
+	res = misc_register(&happyb_dev);
+	if (res < 0) {
+		pr_info("Could not register happyb device");
+		return res;
+	}
+
+
+	return 0;
 }
 
 static void happyb_exit(void)
 {
-        pr_debug("Bye\n");
+	misc_deregister(&happyb_dev);
+	pr_info("Have a nice day, %s\n", name);
 }
 
 module_init(happyb_init);
